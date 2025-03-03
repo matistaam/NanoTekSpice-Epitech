7722c8644d9abb70d88e63e675e15795c8534e32
matis <matis.taam@epitech.eu>
2025-03-03 17:38:55 +0100

    [FIX] output format

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -87,13 +87,13 @@ namespace nts {
         std::cout << "input(s):" << std::endl;
         for (const auto &[name, component] : this->_components) {
             if (dynamic_cast<InputComponent*>(component.get())) {
-                std::cout << "   " << name << ": " << component->compute(1) << std::endl;
+                std::cout << "  " << name << ": " << component->compute(1) << std::endl;
             }
         }
         std::cout << "output(s):" << std::endl;
         for (const auto &[name, component] : this->_components) {
             if (dynamic_cast<OutputComponent*>(component.get()))
-                std::cout << "   " << name << ": " << component->compute(1) << std::endl;
+                std::cout << "  " << name << ": " << component->compute(1) << std::endl;
         }
     }
 
