b41a6a2da8b17144e2ac7b386fe03acf76ee6421
matis <matis.taam@epitech.eu>
2025-03-08 12:36:33 +0100

    [FIX] 4001 Component -> older version

--- include/GatesComponents/Component4001.hpp
+++ include/GatesComponents/Component4001.hpp
@@ -11,14 +11,9 @@
 namespace nts {
     class Component4001 : public AComponent {
         public:
-            Component4001();
-
             Tristate compute(std::size_t pin) override;
 
         private:
             Tristate computeNor(Tristate a, Tristate b);
-            Tristate _q;
-            Tristate _qb;
-            bool _isComputingRS = false;
     };
 }
